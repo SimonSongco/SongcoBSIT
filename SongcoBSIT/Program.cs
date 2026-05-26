f3aa86fda5faf73fb73b7ad7d50d8e0e14991ba4
25-0040c <25-0040c@gmail.com>
2026-05-26 09:46:23 +0800

    Recent Code

--- SongcoBSIT/Program.cs
+++ SongcoBSIT/Program.cs
@@ -11,8 +11,20 @@ namespace SongcoBSIT
         static Dictionary<string,double> CarbonFootprint = new Dictionary<string,double>();
         static Stack<string> EcoActivities = new Stack<string>();
 
+        static Dictionary<string,double> AddCarbon(string name, double value)
+        {
+
+        }
+
+        static Stack<string> AddEcoActivities(string name, double value)
+        {
+
+        }
+
         static void Main(string[] args)
         {
+            bool Loop = true;
+
             CarbonFootprint["Car Commute"] = 25.5;
             CarbonFootprint["Daily Electricity Usage"] = 43.3;
             CarbonFootprint["Daily Waste Disposal"] = 32.4;
@@ -21,17 +33,40 @@ namespace SongcoBSIT
             EcoActivities.Push("Biking to School");
             EcoActivities.Push("Planting Plants in Garden");
 
-            Console.WriteLine("=== Carbon Emmisions ===");
-            foreach (var item in CarbonFootprint)
+            while (Loop)
             {
-                Console.WriteLine($"{item.Key}: {item.Value} KG in CO2");
-            }
+                Console.WriteLine("=== Carbon Emmisions ===");
+                foreach (var item in CarbonFootprint)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value} KG in CO2");
+                }
 
-            Console.WriteLine();
-            Console.WriteLine("=== Recent Eco Activities ===");
-            foreach (var item in EcoActivities)
-            {
-                Console.WriteLine(item);
+                Console.WriteLine();
+                Console.WriteLine("=== Recent Eco Activities ===");
+                int Count = 1;
+                foreach (var item in EcoActivities)
+                {
+                    Console.WriteLine($"[{Count}] {item}");
+                    Count++;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("== Choose Which to Add");
+                Console.WriteLine("[1] Carbon Emmisions");
+                Console.WriteLine("[2] Eco Activities");
+                Console.Write("Enter Your Choice: ");
+                string Choice = Console.ReadLine();
+
+                switch (Choice)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    default:
+                        Loop = false;
+                        break;
+                }
             }
 
             Console.ReadKey();
