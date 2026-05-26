0e79adff2f09da3c6d1b51030825ad36c9c55453
25-0040c <25-0040c@gmail.com>
2026-05-26 09:33:27 +0800

    App SDG

--- SongcoBSIT/Program.cs
+++ SongcoBSIT/Program.cs
@@ -8,9 +8,33 @@ namespace SongcoBSIT
 {
     internal class Program
     {
+        static Dictionary<string,double> CarbonFootprint = new Dictionary<string,double>();
+        static Stack<string> EcoActivities = new Stack<string>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Changed Lines");
+            CarbonFootprint["Car Commute"] = 25.5;
+            CarbonFootprint["Daily Electricity Usage"] = 43.3;
+            CarbonFootprint["Daily Waste Disposal"] = 32.4;
+
+            EcoActivities.Push("Recycling");
+            EcoActivities.Push("Biking to School");
+            EcoActivities.Push("Planting Plants in Garden");
+
+            Console.WriteLine("=== Carbon Emmisions ===");
+            foreach (var item in CarbonFootprint)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} KG in CO2");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=== Recent Eco Activities ===");
+            foreach (var item in EcoActivities)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
         }
     }
 }
